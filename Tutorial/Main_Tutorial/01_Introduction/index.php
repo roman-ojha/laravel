38ae1156755e353da5af28fc61af8e5e3efc97b8
Roman-Ojha <razzroman98@gmail.com>
2022-11-14 22:00:32 +0545

    docs(tutorial): Introduction

---
+++ Tutorial/Main_Tutorial/01_Introduction/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    /*
+        *) What is Laravel?
+            -> PHP framework.
+            -> For developing web app and API.
+            -> Free of const.
+            -> Modern framework and easy to use.
+            -> The most use framework in PHP.
+            -> First release 'June 2011'
+            -> Strong command line support
+            -> Large community
+            -> Regular Update
+            -> Fast and simple
+    */
