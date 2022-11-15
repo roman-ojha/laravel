98dd059264ca3e3c6eae14408e9496ff7e026ef8
Roman-Ojha <razzroman98@gmail.com>
2022-11-15 13:59:24 +0545

    build: For HTMl form and Get value

---
+++ Tutorial/Main_Tutorial/10_Html_form_and_Get_value/note.php
@@ -0,0 +1,21 @@
+<?php
+
+    /*
+        *) Blade Template
+            -> laravel template engine
+            -> convert plain text int php text
+            -> {{ }} is better then <?php ?> to write php
+            -> provide template so, that we can use it as component as well
+            -> can easily use javascript and jquery
+        *) Blade Template expression
+        *) Conditions
+        *) for and Foreach Loop
+        *) Include View in view
+        *) Php in Js
+        *) Csrf Token
+            -> we will learn about these in 'users.blade.php' file
+        *) Header and Footer
+        *) Interview Question
+            1. can we use php tab inside blade template?
+            2. what is the best way to add bootstrap or jquery into laravel
+    */
