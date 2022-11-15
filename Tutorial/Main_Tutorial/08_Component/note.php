4b012d2c47a2b124f100f6fdad649270946753dd
Roman-Ojha <razzroman98@gmail.com>
2022-11-15 12:17:52 +0545

    build(tutorial): Init For Laravel Component

---
+++ Tutorial/Main_Tutorial/08_Component/note.php
@@ -0,0 +1,18 @@
+<?php
+
+    /*
+        *) What is View:
+            -> We put complete HTML part in here
+            -> we use blade template engine for fast rendering
+        *) Make View
+            -> we can create view inside './resources/views'
+        *) Call View
+            -> and we can call it from routing './routes/web.php'
+            -> also we can call view from controller
+            -> we will use './app/Http/Controllers/UserControllers.php'
+        *) Pass Data in View
+            -> We can pass url parameter as data and get access by controller and then can pass it to View
+            -> also we can pass any kind of data from controller to View
+        *) Question?
+            1. Why we create Controller from Command line but not View
+    */
