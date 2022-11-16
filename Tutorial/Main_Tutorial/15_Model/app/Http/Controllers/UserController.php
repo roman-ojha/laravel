5a410181771e9dbc0a817cdc83c55e15d48f35d9
Roman-Ojha <razzroman98@gmail.com>
2022-11-16 09:39:53 +0545

    build: Init Project for Laravel Model

---
+++ Tutorial/Main_Tutorial/15_Model/app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return DB::select("SELECT * FROM students");
+    }
+}
---
+++ Tutorial/Main_Tutorial/15_Model/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserController;
+
+Route::get('/', [UserController::class,'index']);
+
+Route::view('user', 'user');
