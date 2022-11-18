7b60a3b8ee8c7feeaccde07c83d9aa8501854104
Roman-Ojha <razzroman98@gmail.com>
2022-11-18 23:46:07 +0545

    build: Init Project For Save Data in Database

---
+++ Tutorial/Main_Tutorial/24_Save_Data_in_Database/note.php
@@ -0,0 +1,9 @@
+<?php
+
+    /*
+        *) Make Html Form in View
+        *) Make Controller and Model
+        *) Make Route
+        *) Save Data in Table
+        *) Question
+    */
---
+++ Tutorial/Main_Tutorial/24_Save_Data_in_Database/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
+
+use App\Http\Controllers\StudentController;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+// using '/students' route to render View by passing Model Data using controller
+Route::get('students', [StudentController::class,'showStudents']);
