c1e2d489be6a7c9c3cc8b50ee167e1ef42e9f48e
Roman-Ojha <razzroman98@gmail.com>
2022-11-19 22:09:34 +0545

    build: Init Project for Laravel Accessors

---
+++ Tutorial/Main_Tutorial/35_Accessors/note.php
@@ -0,0 +1,11 @@
+<?php
+
+    /*
+        *) What is Accessor
+            -> when we fetch the data from the database and before displaying View template if you want to modify data into database in that case we will use Accessor
+        *) Make Controller
+            ->
+        *) Make Model
+        *) Make a Accessor Function
+        *) Question:
+    */
