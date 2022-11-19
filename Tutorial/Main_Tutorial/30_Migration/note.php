0d0593a23bec292a9088c5c1a5104f8ee670b170
Roman-Ojha <razzroman98@gmail.com>
2022-11-19 20:15:50 +0545

    docs: learnd Important Migration command

--- Tutorial/Main_Tutorial/30_Migration/note.php
+++ Tutorial/Main_Tutorial/30_Migration/note.php
@@ -24,9 +24,31 @@
                     $table->timestamps();
                 });
         *) Migrate Database
-                -> php artisan migrate
-                -> it will create extra table like 'migrations' table
-                -> 'migrations' table is create by the laravel to maintain the information about the migration that we did in the project
-        *) Interview Question
-            1.
+            -> php artisan migrate
+            -> it will create extra table like 'migrations' table
+            -> 'migrations' table is create by the laravel to maintain the information about the migration that we did in the project
+        *) How to reset migration
+            -> php artisan migrate:reset
+            -> to remove the table that is create from the migration we will use reset command
+        *) Rollback Migration
+            -> php artisan migrate:rollback
+            -> to remove the specific migration
+            -> EX: let's say that we had did migration 7 times then if we use rollback command then it can remove the last migrated migration
+            -> so for that we will fist migrate the table for the first time and the add new table and again we will migrate for the second time
+            -> so if we will use 'reset' command then all the table will get removed
+            -> but if we will use rollback then only the last migration table will get removed
+        *) Rollback steps
+            -> if we want to remove multiple table as per given table that get migrated from first to last then we have to use this:
+            -> php artisan migrate:rollback --step <number_of_table>
+        *) Refresh
+            -> php artisan migrate:refresh
+            -> when we will change the database structure or change or manipulate the tables in that case we have to again refresh
+            -> it will first rollback all the table that get migrated and then will again migrate the table
+        *) Single File Migration
+            -> If you just want to migrate single table then we can use this:
+            -> php artisan migrate --path=/database/migrations/<filename>.php
+            -> EX:
+                -> php artisan migrate --path=/database/migrations/2022_11_19_135216_create_test_table.php
+        *) Question
+            1. How we can migrate 2 table at once
     */
