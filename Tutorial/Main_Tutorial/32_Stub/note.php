00716116e821a287dbe0524439d4591f55d322da
Roman-Ojha <razzroman98@gmail.com>
2022-11-19 21:14:32 +0545

    build: Published laravel Stub

--- Tutorial/Main_Tutorial/32_Stub/note.php
+++ Tutorial/Main_Tutorial/32_Stub/note.php
@@ -2,8 +2,17 @@
 
     /*
         *) Why Use Stub Customization
+            -> Stub is a Skeleton file of our Controller, Model, Component, View, Migration or Seeding
+            -> so, the command that we use to generate Controller, Model, Component and the code that we get by default on those file can be able to determine by Stub
+            -> So, for EX: while we will create controller using command what are the initial code that we get are defined inside the Stub
         *) Run Command
+            -> first we have to public or we can see generate stub file
+            -> php artisan stub:publish
+            -> this will create stub file for all the file that can be customizable
         *) Customize Controller
+            -> so now we will customize controller by editing the file on './stubs/controller.plain.stub'
         *) Customize Model
+            -> we can do the same thing with model as well on 'model.stub' file
         *) Question:
+            1. how we can customize Component Stub?
     */
