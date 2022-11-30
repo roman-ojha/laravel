afee674cb1ca4b74cf5113e5e502b827a28928c2
Roman-Ojha <razzroman98@gmail.com>
2022-11-30 07:55:36 +0545

    feat: added migration about column modifier & chang model attribute

---
+++ Tutorial/Main_Tutorial/30_Migration/database/migrations/2022_11_30_015708_create_laptop_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laptop', function (Blueprint $table) {
+            $table->id();
+            // using Column Modifiers
+            $table->string('model_name');
+            // here we are setting the default value for 'brand_name'
+            $table->string('brand_name')->default('HP');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laptop');
+    }
+};
