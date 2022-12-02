775858e1dc7eb3bb57d0e26e5a8386e32b6bbfe4
Roman-Ojha <razzroman98@gmail.com>
2022-12-02 15:24:00 +0545

    refactor: removed unwanted code from Many to Many relationship

--- Tutorial/Main_Tutorial/51_Many_to_Many_Relationship/routes/web.php
+++ Tutorial/Main_Tutorial/51_Many_to_Many_Relationship/routes/web.php
@@ -1,14 +1,5 @@
 <?php
 
-use App\Http\Controllers\DeploymentController;
-use App\Http\Controllers\LanguageController;
-use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-
-Route::get('add-project/', [ProjectController::class,'add_project']);
-Route::get('add-language/{id}', [LanguageController::class,'add_language']);
-Route::get('add-deployment/{id}', [DeploymentController::class,'add_deployment']);
-
-Route::get('get-deployment/{id}', [DeploymentController::class,'get_deployment']);
