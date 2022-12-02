9e6311d5074d777d96505dac1b9ac51ff2ead33f
Roman-Ojha <razzroman98@gmail.com>
2022-12-02 15:26:28 +0545

    feat(many-to-many): added migration, model & controller

--- Tutorial/Main_Tutorial/51_Many_to_Many_Relationship/note.php
+++ Tutorial/Main_Tutorial/51_Many_to_Many_Relationship/note.php
@@ -1,9 +1,9 @@
 <?php
 
     /*
-        -> php artisan make:model Project -mc
-        -> php artisan make:model Language -mc
-        -> php artisan make:model Deployment -mc
+        -> php artisan make:model Single -mc
+        -> php artisan make:model Song -mc
+        -> php artisan make:model SingerSong -m
 
         -> Used files are Migrations files, 'Car.php' Model, 'Mechanic.php' Model, 'Owner.php' Model, 'MechanicController.php', 'CarController.php', 'OwnerController.php', 'web.php' Route
     */
