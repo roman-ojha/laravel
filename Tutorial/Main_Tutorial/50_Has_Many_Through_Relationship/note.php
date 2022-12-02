b0290e05782667c9864ed5b6ba6c919c28b81ab9
Roman-Ojha <razzroman98@gmail.com>
2022-12-02 14:23:42 +0545

    feat(hasManyThrough): added Project, Language, Deployment Model, Migration, Controller

--- Tutorial/Main_Tutorial/50_Has_Many_Through_Relationship/note.php
+++ Tutorial/Main_Tutorial/50_Has_Many_Through_Relationship/note.php
@@ -1,9 +1,9 @@
 <?php
 
     /*
-        -> php artisan make:model Mechanic -mc
-        -> php artisan make:model Car -mc
-        -> php artisan make:model Owner -mc
+        -> php artisan make:model Project -mc
+        -> php artisan make:model Language -mc
+        -> php artisan make:model Deployment -mc
 
         -> Used files are Migrations files, 'Car.php' Model, 'Mechanic.php' Model, 'Owner.php' Model, 'MechanicController.php', 'CarController.php', 'OwnerController.php', 'web.php' Route
     */
