643f305fb48f0406c2aa53485bd36f10457b3cb0
Roman-Ojha <razzroman98@gmail.com>
2022-12-02 15:37:43 +0545

    feat(many-to-many): defined columns on Migrations

--- Tutorial/Main_Tutorial/51_Many_to_Many_Relationship/database/migrations/2022_12_02_094235_create_singer_songs_table.php
+++ Tutorial/Main_Tutorial/51_Many_to_Many_Relationship/database/migrations/2022_12_02_094235_create_singer_songs_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,8 +12,21 @@ return new class extends Migration
      */
     public function up()
     {
+        // creating an intermediate table that will connect 'Singer' and 'Song' to make Many to Many relationship
         Schema::create('singer_songs', function (Blueprint $table) {
             $table->id();
+
+            // creating two foreign key to make relation with 'Singer' & 'Song' Model
+            // creating relation with 'Singer' Table
+            $table->unsignedBigInteger('singer_id');
+            $table->foreign('singer_id')->references('id')->on('singers');
+
+            // creating relation with 'Song' Table
+            // $table->unsignedBigInteger('song_id');
+            // $table->foreign('song_id')->references('id')->on('songs');
+
+            // Another way to create foreign key
+            $table->foreignId('song_id')->constrained('songs');
             $table->timestamps();
         });
     }
