aaf91460905437a6d901551369bae0c5d523f212
Roman-Ojha <razzroman98@gmail.com>
2022-12-02 16:33:14 +0545

    refactor: remove unwanted code from Authentication and Breeze

--- Tutorial/Main_Tutorial/52_Authentication_and_Breeze/note.php
+++ Tutorial/Main_Tutorial/52_Authentication_and_Breeze/note.php
@@ -1,9 +1,4 @@
 <?php
 
     /*
-        -> php artisan make:model Singer -mc
-        -> php artisan make:model Song -mc
-        -> php artisan make:model SingerSong -m
-
-        -> Used files are Migrations files, 'Singer.php' Model, 'Song.php' Model, 'SingerSong.php' Model, 'SingerController.php', 'SongController.php', 'web.php' Route
     */
--- Tutorial/Main_Tutorial/52_Authentication_and_Breeze/routes/web.php
+++ Tutorial/Main_Tutorial/52_Authentication_and_Breeze/routes/web.php
@@ -5,9 +5,3 @@ use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-
-Route::get('add-song/', [SongController::class,'add_song']);
-Route::get('add-singer/', [SingerController::class,'add_singer']);
-
-Route::get('get-song/{id}', [SongController::class,'get_song']);
-Route::get('get-singer/{id}', [SingerController::class,'get_singer']);
