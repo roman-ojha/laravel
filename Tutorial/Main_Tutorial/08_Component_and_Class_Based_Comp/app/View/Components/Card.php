e8c4fa487bf5fffd50b6b1bf03d0714ea6ebced1
Roman-Ojha <razzroman98@gmail.com>
2022-12-06 17:35:32 +0545

    feat: created and defined component variable on class & then displayed

---
+++ Tutorial/Main_Tutorial/08_Component_and_Class_Based_Comp/app/View/Components/Card.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Card extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public $title;
+    public $subTitle;
+    public $description;
+    public function __construct($title, $subTitle, $description)
+    {
+        $this->title=$title;
+        $this->subTitle=$subTitle;
+        $this->description=$description;
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.card');
+    }
+}
---
+++ Tutorial/Main_Tutorial/08_Component_and_Class_Based_Comp/resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h3>Card Title {{ $title }}</h3>
+    <h3>Card Subtitle {{ $subTitle }}</h3>
+    <h3>Card {{ $description }}</h3>
+</div>
