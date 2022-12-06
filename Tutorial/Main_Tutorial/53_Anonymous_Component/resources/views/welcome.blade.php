947ea8f26daeae3981b97e7997a2f6afcd14604e
Roman-Ojha <razzroman98@gmail.com>
2022-12-06 17:57:21 +0545

    feat: created and rendered anonymous component

--- Tutorial/Main_Tutorial/53_Anonymous_Component/resources/views/welcome.blade.php
+++ Tutorial/Main_Tutorial/53_Anonymous_Component/resources/views/welcome.blade.php
@@ -11,17 +11,8 @@
 <body>
     <h1>Welcome Page</h1>
 
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
-
-    @php
-        $desc = 'Description';
-    @endphp
-
-    <x-card title="1" sub-title="roman" :description=$desc class="myclass" />
-    <x-card title="2" sub-title="jack" :description=$desc class="myclass" />
-    <x-card title="3" sub-title="krishna" :description=$desc />
-    <x-card title="4" sub-title="yashodha" :description=$desc />
+    {{-- Rendering anonymous component --}}
+    <x-card title="Hello" subTitle="world" description="Description" />
 </body>
 
 </html>
