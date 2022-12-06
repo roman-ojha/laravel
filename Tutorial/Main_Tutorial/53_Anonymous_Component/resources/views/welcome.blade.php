a5664a1b8e75053e4446c3e717034c6ea91058c6
Roman-Ojha <razzroman98@gmail.com>
2022-12-06 17:49:57 +0545

    build: Init for Anonymous Component

---
+++ Tutorial/Main_Tutorial/53_Anonymous_Component/resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Welcome Page</h1>
+
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+
+    @php
+        $desc = 'Description';
+    @endphp
+
+    <x-card title="1" sub-title="roman" :description=$desc class="myclass" />
+    <x-card title="2" sub-title="jack" :description=$desc class="myclass" />
+    <x-card title="3" sub-title="krishna" :description=$desc />
+    <x-card title="4" sub-title="yashodha" :description=$desc />
+</body>
+
+</html>
---
+++ Tutorial/Main_Tutorial/53_Anonymous_Component/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserController;
+
+Route::get('/user/{id}', [UserController::class,'show']);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/users/{username}', [UserController::class,'loadUserView']);
+
+
+Route::view('about', '/about');
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/abouts', function () {
+    return redirect('/about');
+});
+
+Route::get('/{name}', function ($name) {
+    return view('dynamic', ['name'=>$name]);
+});
