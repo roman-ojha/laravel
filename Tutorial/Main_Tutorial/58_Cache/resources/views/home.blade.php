d0c7a2f67be2acae768632b8c5cf10502562612e
Roman-Ojha <razzroman98@gmail.com>
2022-12-07 19:35:36 +0545

    docs: related to laravel Cache

--- Tutorial/Main_Tutorial/58_Cache/resources/views/home.blade.php
+++ Tutorial/Main_Tutorial/58_Cache/resources/views/home.blade.php
@@ -11,23 +11,6 @@
 <body>
     <h1>Home Page</h1>
 
-    <ul>
-        <li>
-            {{-- using 'route' method to redirect using named route --}}
-            <a href="{{ route('home') }}">Home</a>
-        </li>
-        <li>
-            <a href="{{ route('about') }}">About</a>
-        </li>
-        {{-- we can pass the dynamic value into the dynamic urls --}}
-        <li>
-            <a href="{{ route('redirect', ['page' => 'home']) }}">Home</a>
-        </li>
-        <li>
-            <a href="{{ route('redirect', ['page' => 'about']) }}">About</a>
-            {{-- http://localhost:8000/redirect/about?pg=3 --}}
-        </li>
-    </ul>
 </body>
 
 </html>
--- Tutorial/Main_Tutorial/58_Cache/routes/web.php
+++ Tutorial/Main_Tutorial/58_Cache/routes/web.php
@@ -6,20 +6,4 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('home');
-})->name('home');
-// we can add name for every route
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-// now we can use these name to navigate to these routes
-
-Route::get('/redirect/{page}', function ($page) {
-    // we can use controller as well to redirect to the named route
-    if ($page=='home') {
-        return redirect()->route('home');
-    } elseif ($page =='about') {
-        return redirect()->route('about');
-    }
-    return "Not Found";
-})->name('redirect');
+});
