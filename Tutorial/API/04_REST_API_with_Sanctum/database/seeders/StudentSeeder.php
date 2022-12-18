3089acc822750ea6e78f17494d036f5bd2dd44df
Roman-Ojha <razzroman98@gmail.com>
2022-12-18 14:59:13 +0545

    feat(sanctum-api): added new filed onto Student & add data using seeder

--- Tutorial/API/04_REST_API_with_Sanctum/database/seeders/StudentSeeder.php
+++ Tutorial/API/04_REST_API_with_Sanctum/database/seeders/StudentSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class StudentSeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+        foreach (range(1, 10) as $value) {
+            DB::table('students')->insert([
+                'name'=>$faker->name(),
+                'city'=>$faker->city(),
+                'fees'=>$faker->randomFloat(2, 0, 10000),
+            ]);
+        }
     }
 }
